c6f5a741ff9872e523ce0017bf583865b65b35af
Arthur Weale <ajayrossy340@gmail.com>
2025-11-26 04:01:05 +0200

    Added screens to the navigation stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,10 @@ export default function RootLayout() {
         <Stack.Screen name="index"/>
         <Stack.Screen name="screens/DeckDetails"/>
         <Stack.Screen name="screens/CreateDeck"/>
+        <Stack.Screen name="screens/AddCardScreen"/>
+        <Stack.Screen name="screens/FlashCards"/>
+        <Stack.Screen name="screens/Session"/>
+        <Stack.Screen name="screens/Stats"/>
       </Stack>
   );
 }
