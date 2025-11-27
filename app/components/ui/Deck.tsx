6e485243ef1092199bba8e80157cce2b7d94d2df
Arthur Weale <ajayrossy340@gmail.com>
2025-11-27 18:20:12 +0200

    Changed and add dummy data from utils

--- app/components/ui/Deck.tsx
+++ app/components/ui/Deck.tsx
@@ -2,54 +2,27 @@ import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { LinearGradient }  from "expo-linear-gradient";
 import { useRouter } from 'expo-router';
+import safeNavigate from '@/app/utils/safeNavigate';
+import { dummyDecks } from '@/app/data/dummydata';
 
-interface Decks{
-    id: string,
-    title: string,
-    cards: number, 
-    color: [string, string]
-}
 
-const decks: Decks[] = [
-  {id: "1",
-    title: "Spanish",
-    cards: 30, 
-    color: ["#60a5fa", "#3b82f6"]
-  },
-
-  {id: "2",
-    title: "Science",
-    cards: 30,
-    color: ["#F472B6", "#EC4899"]
-  },
-
-  {id: "3",
-    title: "Javascript Basics",
-    cards: 30,
-    color: ["#FDE047", "#FACC15"]
-  },
-    {id: "4",
-    title: "Science",
-    cards: 30,
-    color: ["#C084FC", "#A855F7"]
-  },
-]
+const decks = dummyDecks;
 
 const Deck = () => {
   const route = useRouter()
   return (
         <FlatList
             data={decks}
-            keyExtractor={(item)=> item.id}
+            keyExtractor={(item)=> item.id.toString()}
             numColumns={2}
             columnWrapperStyle={{ justifyContent: "space-between", marginBottom: 10 }}
             renderItem={({item})=> <TouchableOpacity className={`w-[48%] min-h-[140px] rounded-2xl shadow-card-lg transition-all transform overflow-hidden`}
-            onPress={()=> route.push(`/screens/DeckDetails?id=${item.id}&title=${item.title}&cards=${item.cards}`)}>
+            onPress={()=> safeNavigate(()=> route.push(`/screens/DeckDetails?id=${item.id}&title=${item.title}&cards=${item.cards}`))}>
             <LinearGradient
-            colors={item.color}
+            colors={item.color as [string, string]}
             start={{x: 0, y:0}}
             end={{x: 0.2, y: 1}}
-            className="flex-1 p-5 justify-start"
+            className="flex-1 p-5 justify-between"
             >
                 {/* Blobs */}
                 <View style={{ position:'absolute', top:-20, right:-20, width:80, height:80, borderRadius:40, backgroundColor:'#93C5FD', opacity:0.3 }} />
@@ -58,8 +31,8 @@ const Deck = () => {
                 <View style={{ position:'absolute', bottom:-10, left:-10, width:30, height:30, borderRadius:15, backgroundColor:'#FECFEF', opacity:0.2 }} />
                 <View style={{ position: 'absolute', bottom: 20, left: 105, width: 50, height: 50, borderRadius: 25, backgroundColor: '#FBCFE8', opacity:0.25 }}/>
 
-                <Text>{item.title}</Text>
-                <Text>{item.cards}</Text>
+                <Text className='text-white'>{item.title}</Text>
+                <Text className='text-white'>{item.cards.length} Cards</Text>
             </LinearGradient>
             </TouchableOpacity>}
         />
